dda700183d76a11f74f5a9e268fffb41afe02ef5
Rodrigo Castro <18582098+rodrcastro@users.noreply.github.com>
2025-01-30 16:08:25 +0000

    further repo cleaning

--- tests/index.test.ts
+++
@@ -1,5 +0,0 @@
-describe("currently doesn't test anything as this file is highly integrated into the gitbook runtime", () => {
-  it("life's beauty without logical conflict", () => {
-    expect(true).toBeTruthy();
-  });
-});
